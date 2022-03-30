5779d22198d9f8fd8877ed225b8d55fb270cb959
Kaat Willems <97091300+KaatWillems@users.noreply.github.com>
2022-03-30 12:13:31 +0200

    did nothing. dont copy this code

--- app_setup/models/post.js
+++ app_setup/models/post.js
@@ -1,26 +1,26 @@
-// Kaat: SHOULD BE DELETED BUT I KEEP TO KEEP APP RUNNING FOR NOW
-const mongoose = require('mongoose');
-const PostSchema  = new mongoose.Schema({
-  title :{
-      type  : String,
-      required : true
-  } ,
-  post_content :{
-    type  : String,
-    required : true
-  } ,
-  url: {
-    type  : String,
-    required : true
-  },
-  date :{
-    type : Date,
-    default : Date.now
-  }
-});
+// // Kaat: SHOULD BE DELETED BUT I KEEP TO KEEP APP RUNNING FOR NOW
+// const mongoose = require('mongoose');
+// const PostSchema  = new mongoose.Schema({
+//   title :{
+//       type  : String,
+//       required : true
+//   } ,
+//   post_content :{
+//     type  : String,
+//     required : true
+//   } ,
+//   url: {
+//     type  : String,
+//     required : true
+//   },
+//   date :{
+//     type : Date,
+//     default : Date.now
+//   }
+// });
 
-const Post = mongoose.model('Post',PostSchema);
+// const Post = mongoose.model('Post',PostSchema);
 
-module.exports = {Post, PostSchema};
+// module.exports = {Post, PostSchema};
 
 
--- app_setup/routes/index.js
+++ app_setup/routes/index.js
@@ -4,31 +4,36 @@ const {ensureAuthenticated} = require('../config/auth')
 const Profile = require("../models/profile").Profile;
 const Post = require("../models/post").Post
 
-//login page
-router.get('/', (req,res)=>{
-  Post.find({}, (err, allPosts) => {
-    res.render('welcome', {allposts: allPosts });
-  })
+//login page diogo 
+// router.get('/', (req,res)=>{
+//   Post.find({}, (err, allPosts) => {
+//     res.render('welcome', {allposts: allPosts });
+//   })
+// })
+
+//login page set the bar 
+router.get('/', (req,res)=>{  
+    res.render('welcome');  
 })
+
 //register page
 router.get('/register', (req,res)=>{
     res.render('register');
 })
 
+//diogo 
+// const getProfileAndPopulate = function(id){
+//   return Profile.findById(id).populate("posts")
+// }
 
-const getProfileAndPopulate = function(id){
-  return Profile.findById(id).populate("posts")
-}
+// const renderDashboardWithPosts = async function(req, res){
+//   posts = await getProfileAndPopulate(req.user.profile._id)
 
-
-const renderDashboardWithPosts = async function(req, res){
-  posts = await getProfileAndPopulate(req.user.profile._id)
-
-  res.render('dashboard',{
-      user: req.user,
-      posts: posts
-  });
-}
+//   res.render('dashboard',{
+//       user: req.user,
+//       posts: posts
+//   });
+// }
 
 
 
