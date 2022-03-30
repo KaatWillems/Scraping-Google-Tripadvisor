c18e231b87e2bfbed0a63327b669148cb5c9d883
Kaat Willems <97091300+KaatWillems@users.noreply.github.com>
2022-03-30 10:29:50 +0200

    finished the schemas in models folder. not yet linked in routes

---
+++ app_setup/models/bar.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const BarSchema  = new mongoose.Schema({
+  name :{
+      type  : String,
+      required : true
+  } ,
+  addressCity :{
+    type  : String,
+    required : true
+  } ,
+  addressStreet: {
+    type  : String,
+    required : true
+  },
+  addressNumber: {
+    type  : String,   // I put string because sometimes the housenumber can contain letters 
+    required : true
+  },
+  siteUrl: {
+    type  : String,
+  },
+  schedule: {
+    type  : String,
+  },
+  pictureUrl: {
+    type  : String,
+  },
+  ratings: {
+    type  : Number,
+  },
+  reviews : [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review"
+    }
+  ]
+});
+
+const Bar = mongoose.model('Bar',BarSchema);
+
+module.exports = {Bar, BarSchema};
+
