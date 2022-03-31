4377867a319e564b782ea32e39c7500adb52c2b2
Anthony Hoyas <anthony.hoyas@gmail.com>
2022-03-31 17:03:44 +0200

    pushgin today's work not fiiseh

--- app_setup/public/scripts/searchProfile.js
+++ app_setup/public/scripts/searchProfile.js
@@ -1,39 +1,6 @@
-const searchBar = document.getElementById('search_bar')
-const sugg = document.querySelector('.suggestions')
+const searchbar2 = document.getElementById('test1')
 
-searchBar.addEventListener('keyup', (event) => {
-  if(!searchBar.value == ""){
-    searchProfiles(searchBar.value)
-  }else{
-    sugg.innerHTML = ""
-  }
-
+searchbar2.addEventListener('click', () =>{
+  console.log ('test')
 })
 
-
-const searchProfiles = (input) => {
-  fetch("profiles/search", {
-    method:'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({user_input: input})
-  })
-  .then(response => response.json())
-  .then(data => {
-    sugg.innerHTML = ""
-    data.data.forEach((profile) => {
-      sugg.insertAdjacentHTML('beforeend', `
-        <a href="/profiles/show/${profile._id}">
-          <div class="suggestion">
-            <div class="avatar"></div>
-            <div class="name">${profile.username}</div>
-          </div>
-        </a>
-        `)
-    })
-  })
-  .catch((err) =>{
-    console.log(err)
-  })
-}
--- app_setup/routes/bars.js
+++ app_setup/routes/bars.js
@@ -2,4 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Profile = require("../models/profile").Profile;
 const User = require("../models/user")
+const Bar = require("..models/bars").Bar;
 // const cloudinary = require("cloudinary");
+
+router.post("/search", async (req, res) => {
+    console.log(req.body)
+    const profiles = await Profile.find({"username": {$regex: req.body.user_input}})
+    res.send({data: profiles})
+  
+  })
