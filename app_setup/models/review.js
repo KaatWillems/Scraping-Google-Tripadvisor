6aa9c8c09979559963bc137e99ce681476ce1876
Kaat Willems <97091300+KaatWillems@users.noreply.github.com>
2022-03-31 09:02:11 +0200

    update review (matching with models2 branch)

--- app_setup/models/review.js
+++ app_setup/models/review.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
-=======
 const ProfileSchema = require("./profile").ProfileSchema
 
->>>>>>> 23e5373a46e4af15cb37d54c05ed533dcc91ce05
 const ReviewSchema  = new mongoose.Schema({
   rating :{
       type  : Number,
@@ -33,21 +30,8 @@ const ReviewSchema  = new mongoose.Schema({
     type : Date,
     default : Date.now
   },
-<<<<<<< HEAD
-  bars : [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "bar"
-    }
-  ]
-});
-const Review = mongoose.model('Review',ReviewSchema);
-
-module.exports = {Review, ReviewSchema};
-=======
   profile: ProfileSchema 
 });
 const Review = mongoose.model('Review',ReviewSchema);
 
 module.exports = {Review, ReviewSchema};
->>>>>>> 23e5373a46e4af15cb37d54c05ed533dcc91ce05
