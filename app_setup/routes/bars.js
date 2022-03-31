c5e948bba65ca38d7e4d0690f13c0f06c9609636
Kaat Willems <97091300+KaatWillems@users.noreply.github.com>
2022-03-31 09:16:32 +0200

    start

---
+++ app_setup/routes/bars.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const Profile = require("../models/profile").Profile;
+const User = require("../models/user")
+// const cloudinary = require("cloudinary");
