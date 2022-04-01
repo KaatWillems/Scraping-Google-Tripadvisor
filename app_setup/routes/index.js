14d7c7491de92816a1dfb3f29cb854d462a7db81
Kaat Willems <97091300+KaatWillems@users.noreply.github.com>
2022-04-01 14:42:25 +0200

    updated old index.js to current master one

--- app_setup/routes/index.js
+++ app_setup/routes/index.js
@@ -3,39 +3,88 @@ const router  = express.Router();
 const {ensureAuthenticated} = require('../config/auth')
 const Profile = require("../models/profile").Profile;
 const Post = require("../models/post").Post
+const Bar = require("../models/bar").Bar
 
-//login page
-router.get('/', (req,res)=>{
-  Post.find({}, (err, allPosts) => {
-    res.render('review', {allposts: allPosts }); //change back to welcome insteadof review!!!
-  })
+//login page diogo 
+// router.get('/', (req,res)=>{
+//   Post.find({}, (err, allPosts) => {
+//     res.render('welcome', {allposts: allPosts });
+//   })
+// })
+
+//login page set the bar 
+router.get('/', (req,res)=>{  
+    res.render('welcome');  
 })
+
 //register page
 router.get('/register', (req,res)=>{
     res.render('register');
 })
 
+//diogo 
+// const getProfileAndPopulate = function(id){
+//   return Profile.findById(id).populate("posts")
+// }
 
-const getProfileAndPopulate = function(id){
-  return Profile.findById(id).populate("posts")
+// const renderDashboardWithPosts = async function(req, res){
+//   posts = await getProfileAndPopulate(req.user.profile._id)
+
+//   res.render('dashboard',{
+//       user: req.user,
+//       posts: posts
+//   });
+
+
+// }
+
+
+
+
+
+const getStars = (bar) => { 
+  let rating = bar.ratings;
+  ratingarr = rating.split(',')
+  console.log(ratingarr)
+  starNbr = parseInt(ratingarr[0])
+  console.log(starNbr)
+  let finalArr = []
+  for (let i = 0; i < starNbr; i++) {
+    finalArr.push('*')
+    
+  }
+  if(ratingarr[1]){
+    finalArr.push("/")
+  } else if (!starNbr[1] && starNbr < 5) {
+
+    finalArr.push(" ")
+    }
+
+
+  return finalArr
 }
 
 
-const renderDashboardWithPosts = async function(req, res){
-  posts = await getProfileAndPopulate(req.user.profile._id)
 
-  res.render('dashboard',{
-      user: req.user,
-      posts: posts
-  });
-}
+router.get('/dashboard',ensureAuthenticated, async (req,res)=>{
+    if(!req.user.Profile){
+
+    
+      
 
 
+      bararr = []
 
-router.get('/dashboard',ensureAuthenticated,(req,res)=>{
-    if(!req.user.profile){
+      let bars = await Bar.find()
+      bars.forEach((bar) => {
+        bararr.push({bar: bar, barrating: getStars(bar)})
+      })
+      //console.log(bararr)
+      // let trendingbars
       res.render('dashboard',{
-        user: req.user
+        user: req.user,
+        bars: bararr,
+
       });
     }else{
       renderDashboardWithPosts(req, res)
@@ -43,3 +92,4 @@ router.get('/dashboard',ensureAuthenticated,(req,res)=>{
     }
 })
 module.exports = router;
+//module.exports = getStars()
