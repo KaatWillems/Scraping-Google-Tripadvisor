dde9e6809485e09ae925fa52dcbe6509a4f19eca
sebherrerabe <sebastianherrerac1997@gmail.com>
2022-04-04 16:35:31 +0200

    i did a lot guys

--- app_setup/public/scripts/main.js
+++ app_setup/public/scripts/main.js
@@ -1,2 +1,10 @@
-const navigationFavorites = document.querySelector('.navigation-favorites')
+const searchBar = document.querySelector('.set-the-bar-search-bar'),
+locationDiv = document.querySelector('.location-general-container'),
+dashboardDiv = document.querySelector('.dashboard-container'),
+setTitle = document.querySelector('.set-the-bar-title')
+
+searchBar.addEventListener('click',() => {
+    locationDiv.style.display = 'flex'
+    dashboardDiv.style.display = 'none'
+})
 
